83a6e529e7799c2053f029be455ee33330d0cddb
sikithedev <muaaz.sikiric@gmail.com>
2025-09-18 21:03:17 +0200

    Prevent publishing form that has no input elements

---
+++ components/forms/ui/PublishFormButton.tsx
@@ -0,0 +1,82 @@
+import { publishFormById } from "@/actions/forms";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import useDesigner from "@/hooks/useDesigner";
+import { formElementsByCategory } from "@/types/formElements";
+import { LoaderCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
+type PublishFormButtonProps = {
+  id: number;
+};
+
+export default function PublishFormButton({ id }: PublishFormButtonProps) {
+  const router = useRouter();
+  const { elements } = useDesigner();
+  const [isPending, startTransition] = useTransition();
+
+  async function publishForm() {
+    try {
+      if (
+        !elements.some((el) =>
+          formElementsByCategory["input"].includes(el.type)
+        )
+      ) {
+        toast.error(
+          "Please add at least one input field to the form before publishing."
+        );
+        return;
+      }
+
+      const content = JSON.stringify(elements);
+
+      await publishFormById(id, content);
+      toast.success("Form published successfully");
+      router.refresh();
+    } catch (error) {
+      toast.error("Failed to publish form. Please try again.");
+    }
+  }
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button>Publish</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. Once published, you will not be able
+            to edit the form.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={(e) => {
+              e.preventDefault();
+              startTransition(publishForm);
+            }}
+            disabled={isPending}
+            className="min-w-24"
+          >
+            {isPending ? <LoaderCircle className="animate-spin" /> : "Continue"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
