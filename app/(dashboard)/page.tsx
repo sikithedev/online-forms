62f60668d49cb8946526c8bf4da880702abccf65
sikithedev <muaaz.sikiric@gmail.com>
2025-09-21 20:41:51 +0200

    Add form limit per user

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-import { getFormStats } from "@/actions/forms";
+import { getForms, getFormStats } from "@/actions/forms";
 import FormCards from "@/components/forms/dashboard/FormCards";
 import StatsCards from "@/components/forms/dashboard/StatsCards";
 import CreateFormButton from "@/components/forms/ui/CreateFormButton";
@@ -6,17 +6,28 @@ import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
   const { visits, submissions } = await getFormStats();
+  const forms = await getForms();
+
+  const formLimit = 10;
 
   return (
     <div className="container mx-auto space-y-4 px-4">
       <StatsCards visits={visits} submissions={submissions} allForms />
       <Separator />
       <div className="flex justify-between items-center">
-        <h2 className="text-3xl font-semibold tracking-tight">Your forms</h2>
-        <CreateFormButton />
+        <h2 className="text-3xl font-semibold tracking-tight">
+          Your forms{" "}
+          <span className="text-muted-foreground text-xl">
+            ({forms.length}/{formLimit})
+          </span>
+        </h2>
+        <CreateFormButton
+          formLimit={formLimit}
+          disabled={forms.length >= formLimit}
+        />
       </div>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <FormCards />
+        <FormCards forms={forms} />
       </div>
     </div>
   );
--- components/forms/ui/CreateFormButton.tsx
+++ components/forms/ui/CreateFormButton.tsx
@@ -21,6 +21,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { formSchema } from "@/schemas/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoaderCircle } from "lucide-react";
@@ -29,7 +34,15 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
-export default function CreateFormButton() {
+type CreateFormButtonProps = {
+  formLimit: number;
+  disabled: boolean;
+};
+
+export default function CreateFormButton({
+  formLimit,
+  disabled,
+}: CreateFormButtonProps) {
   const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -42,7 +55,7 @@ export default function CreateFormButton() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-      const formId = await createForm(values);
+      const formId = await createForm(values, formLimit);
       toast.success("Form created successfully");
       router.push(`/builder/${formId}`);
     } catch (error) {
@@ -56,9 +69,27 @@ export default function CreateFormButton() {
 
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button>Create new form</Button>
-      </DialogTrigger>
+      {disabled ? (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span className="cursor-not-allowed">
+              <Button disabled className="pointer-events-none">
+                Create new form
+              </Button>
+            </span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="max-w-60">
+              You've reached your form limit. Delete an existing form to create
+              a new one.
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      ) : (
+        <DialogTrigger asChild>
+          <Button>Create new form</Button>
+        </DialogTrigger>
+      )}
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Create form</DialogTitle>
